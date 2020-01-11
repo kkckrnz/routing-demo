63781516740e0e1b5b07d0c0e893201b706b56ad
Ken Choi <kkckrnz@gmail.com>
2020-01-11 17:01:40 +1300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './components/department-list/department-list.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'departments', pathMatch: 'full'},
   { path: 'departments', component: DepartmentListComponent},
   { path: 'employees', component: EmployeeListComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
